7378d41239ad65d5ffd37f3fba137ccd5d7557b1
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-11-04 00:03:06 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,7 @@
 class Book
-  attr_accessor :title, :author, :genre
+  attr_accessor :title, :author, :genre, :page_count
   def initialize(title)
     @title = title
-  end
-  def page_count=(page_count)
-    @page_count = page_count
-  end
-  def page_count
-    @page_count
   end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
